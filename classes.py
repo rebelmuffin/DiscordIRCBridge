8e57339b5eeec03c1d516f4bcd0375380419f150
Rebel Muffin <bakirciselim@yahoo.com.tr>
2021-06-15 23:45:34 +0300

    Initial commit

---
+++ classes.py
@@ -0,0 +1,165 @@
+import asyncio
+import json
+import sys
+from typing import Union, Optional
+
+import discord
+from discord.ext import commands
+import irc.bot
+import irc.strings
+
+
+MAX_NOTICE_LENGTH = 512
+
+
+class IRCBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, nickname, realname, server, port=6667):
+        super().__init__([(server, port)], nickname, realname)
+        self.loop = asyncio.new_event_loop()
+        self.discord_bot: Optional[commands.Bot] = None
+        self.discord_channels = irc.bot.IRCDict()
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        print(f"IRC {self.connection.get_nickname()} ready")
+        # Join general
+        c.join("#general")
+        # Load channel binds
+        with open("channels.json", "r") as file:
+            channels = json.loads(file.read())
+        for channel in channels:
+            self.bind_channel(channel, channels[channel])
+
+    def on_privmsg(self, c, e):
+        self.process_command(e, e.arguments[0])
+
+    def process_message(self, channel: Union[discord.GroupChannel, discord.DMChannel, discord.TextChannel],
+            message: discord.Message, content_prefix: str = "", content_suffix: str = ""):
+        """Processes a message that came to discord bot"""
+        c = self.connection
+
+        # Get bound IRC channel
+        channel_id = channel.id
+        irc_channel = self.get_channel_from_discord(channel_id)
+        if not irc_channel:
+           return 
+
+        # Send message into the bound IRC channel
+        attachments = ' '.join([a.url for a in message.attachments])
+        #print(f"[IRC] <{message.author} ({message.author.id})> \"{message.content}\"")
+        #sys.stdout.flush()
+
+        # Clean the content and wrap it correctly for IRC notice compatibility
+        content_lines = (content_prefix + str(message.clean_content) + content_suffix).split("\n")
+        content_lines_paginated = []
+        for line in content_lines:
+            pages = [line[i:i+MAX_NOTICE_LENGTH] for i in range(0, len(line), MAX_NOTICE_LENGTH)]
+            if len(pages) > 1:
+                content_lines_paginated = content_lines_paginated + pages
+            else:
+                content_lines_paginated.append(line)
+
+        if isinstance(channel, discord.DMChannel):
+            for line in content_lines_paginated:
+                c.notice(irc_channel, line)
+            if attachments:
+                c.notice(irc_channel, attachments)
+        else:
+            for line in content_lines_paginated:
+                c.notice(irc_channel, f"<{message.author}> {line}")
+            if attachments:
+                c.notice(irc_channel, f"<{message.author}> {attachments}")
+
+    def on_pubmsg(self, c, e):
+        if self.discord_bot is None:
+            return
+        discord_channel = self.get_discord_channel(self.discord_channels.get(e.target))
+        self.discord_bot.loop.create_task(discord_channel.send(" ".join(e.arguments)))
+
+    def get_channel_from_discord(self, channel_id: int):
+        """Gets IRC channel name from discord channel ID"""
+        inverted = {v: k for k, v in self.discord_channels.items()}
+        return inverted.get(channel_id)
+
+    def get_channel_from_irc(self, channel_name: str):
+        """Gets discord channel id from IRC channel name"""
+        return self.discord_channels.get(channel_name)
+
+    def get_discord_channel(self, channel_id: int) -> Union[discord.DMChannel, discord.TextChannel, discord.GroupChannel]:
+        """Gets discord channel object from the id"""
+        channel = self.discord_bot.get_channel(channel_id)
+        if not channel:
+            channel = self.discord_bot.get_user(channel_id)
+        return channel
+
+    def bind_channel(self, irc_channel: str, discord_channel_id: int):
+        """Binds IRC channel to discord channel"""
+        self.discord_channels[irc_channel] = discord_channel_id
+        self.connection.join(irc_channel)
+
+    def perm_bind(self, irc_channel: str, discord_channel_id: int):
+        """Permanently binds an IRC channel to discord channel"""
+        # Bind in memory
+        self.bind_channel(irc_channel, discord_channel_id)
+
+        # Write to channels.json
+        with open("channels.json", "r") as f:
+            binds = json.loads(f.read())
+        binds[irc_channel] = int(discord_channel_id)
+        with open("channels.json", "w") as f:
+            f.write(json.dumps(binds))
+
+    def process_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+        args = cmd.split()[1:]
+        cmd = cmd.split()[0]
+
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel Stats ---")
+                c.notice(nick, f"Channel: {chname}")
+                users = sorted(chobj.users())
+                c.notice(nick, f"Users: {', '.join(users)}")
+                opers = sorted(chobj.opers())
+                c.notice(nick, f"Opers: {', '.join(opers)}")
+                voiced = sorted(chobj.voiced())
+                c.notice(nick, f"Voiced: {', '.join(voiced)}")
+        elif cmd == "channels":
+            c.notice(nick, f"Channels: {', '.join([str(ch) for ch in self.channels])}")
+            c.notice(nick, f"Channel Objs: {', '.join([str(ch) for ch in self.channels.items()])}")
+        elif cmd == "test":
+            c.notice(nick, f"Target: {e.target}")
+            c.notice(nick, f"Source: {e.source}")
+            c.notice(nick, f"Args: {str(args)}")
+        elif cmd == "bind":
+            irc_channel = args[0]
+            discord_channel = args[1]
+            try:
+                if not self.get_discord_channel(int(discord_channel)):
+                    return c.notice(nick, "Discord channel not found!")
+                c.join(irc_channel)
+                self.bind_channel(irc_channel, discord_channel)
+                c.notice(nick, "Bind successful")
+            except Exception as e:
+                c.notice(nick, f"Exception: {e}")
+        elif cmd in ["pbind", "permbind", "permabind"]:
+            irc_channel = args[0]
+            discord_channel = args[1]
+            try:
+                irc_channel = "#"+irc_channel if not irc_channel.startswith("#") else irc_channel
+                if not self.get_discord_channel(int(discord_channel)):
+                    return c.notice(nick, "Discord channel not found!")
+                c.join(irc_channel)
+                self.perm_bind(irc_channel, discord_channel)
+                c.notice(nick, "Bind successful")
+            except Exception as e:
+                c.notice(nick, f"Exception: {e}")
+        else:
+            c.notice(nick, f"Unknown Command: {cmd}")
---
+++ main.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+import sys
+import json
+import threading
+
+import discord
+from discord.ext import commands
+
+from classes import IRCBot
+
+
+# Setup discord bot
+discord_bot = commands.Bot("**__", None)
+
+# Load config
+try:
+    with open("config.json", "r") as f:
+        config = json.loads(f.read())
+except FileNotFoundError:
+    print("Bot config file not found!")
+    sys.exit(-1)
+
+
+# Retrieve config data from dict
+TOKEN = config.get("TOKEN")
+IRC_HOST = config.get("IRC_HOST")
+IRC_PORT = config.get("IRC_PORT", 6667)
+IRC_PASS = config.get("IRC_PASS")
+IRC_NICK = config.get("IRC_NICK")
+IRC_REAL = config.get("IRC_REAL")
+
+
+# Setup IRC bot
+irc_bot = IRCBot(IRC_NICK, IRC_REAL, IRC_HOST, IRC_PORT)
+irc_bot.discord_bot = discord_bot
+irc_thread = threading.Thread(target=irc_bot.start)
+irc_thread.start()
+
+
+@discord_bot.event
+async def on_ready():
+    print(f"Discord bot ready on {discord_bot.user}")
+    sys.stdout.flush()
+    with open("channels.json", "r") as file:
+        binds = json.loads(file.read())
+    for irc, dc in binds.items():
+        try:
+            await discord_bot.fetch_channel(int(dc))
+        except discord.NotFound:
+            try:
+                user_profile = await discord_bot.fetch_user_profile(int(dc))
+            except discord.HTTPException:
+                print(f"User or Channel `{dc}` is inaccessible")
+                sys.stdout.flush()
+                continue
+            user = user_profile.user
+            await user.create_dm()
+
+
+@discord_bot.listen()
+async def on_message(msg: discord.Message):
+    await discord_bot.wait_until_ready()
+    if msg.author != discord_bot.user:
+        user_profile = await discord_bot.fetch_user_profile(msg.author.id)
+        msg.author = user_profile.user
+        print(f"[DISCORD] <{msg.author} ({msg.author.id})> \"{msg.content}\"")
+        sys.stdout.flush()
+        irc_bot.process_message(msg.channel, msg)
+
+
+@discord_bot.listen()
+async def on_message_edit(before: discord.Message, after: discord.Message):
+    await discord_bot.wait_until_ready()
+    if after.author != discord_bot.user:
+        user_profile = await discord_bot.fetch_user_profile(after.author.id)
+        after.author = user_profile.user
+        print(f"[DISCORD] <{after.author} ({after.author.id})> \"{after.content}\"")
+        sys.stdout.flush()
+        irc_bot.process_message(after.channel, after, content_prefix="*")
+
+discord_bot.run(TOKEN)
